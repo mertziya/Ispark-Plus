9702e8c50b4d8d83887256cd89bde1fe15553688
mertziya <mert7ziya@gmail.com>
2025-02-09 14:26:24 +0300

    initial commit

---
+++ FastPark/Model/Park.swift
@@ -0,0 +1,19 @@
+//
+//  Location.swift
+//  FastPark
+//
+//  Created by Mert Ziya on 9.02.2025.
+//
+
+import Foundation
+
+struct Park {
+    var parkID: Int?
+    var parkName, lat, lng: String?
+    var capacity, emptyCapacity: Int?
+    var workHours, parkType: String?
+    var freeTime: Int?
+    var district: String?
+    var isOpen: Int?
+}
+
---
+++ FastPark/Model/ParkDetails.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetail.swift
+//  FastPark
+//
+//  Created by Mert Ziya on 9.02.2025.
+//
+
+import Foundation
+
+struct ParkDetail {
+    var locationName: String?
+    var parkID: Int?
+    var parkName, lat, lng: String?
+    var capacity, emptyCapacity: Int?
+    var updateDate, workHours, parkType: String?
+    var freeTime, monthlyFee: Int?
+    var tariff, district, address, areaPolygon: String?
+}
